820651ea63dabf3f776fb544429fce272474eae7
sramunyisi@csir.co.za <sramunyisi@csir.co.za>
2017-01-27 21:27:04 +0200

    added CSS and JavaScript scripts

--- eMphaya/blog/urls.py
+++ eMphaya/blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
+urlpatterns += staticfiles_urlpatterns()
